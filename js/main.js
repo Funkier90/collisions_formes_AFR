93aecdde460e4f5ab2dd429f3baa37b921773e06
Funkier90 <hugo.rob88@hotmail.fr>
2023-03-14 13:17:58 -0400

    Premier commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+function test_collision_rectangles(rect1, rect2) {
+    if (rect1.x - 50 >= rect2.x - 125){
+        return true
+    }else{
+        return false
+    }
+}    
+    
+
+
+function test_collision_cercles(cercle1, cercle2) {
+    return false
+}
